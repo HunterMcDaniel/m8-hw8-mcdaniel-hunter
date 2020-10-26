64df083a6dde87c3bb2da0d02ce81489b313db26
“Hunter <hunterlmcdaniel@ufl.edu>
2020-10-25 23:33:51 -0400

    Try to call function

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// It is always helpful to use comments in your code!
+var dropdowns = document.getElementsByClassName('dropdown');
+//console.log(dropdowns);
+
+var menuDrop = document.getElementsByClassName('nav-link');
+// console.log(menuDrop);
+
+function toggleDropdown() {
+console.log(dropdowns[0]);
+ dropdowns[0].classList.add('show');
+}
+// }
+// for( i = 0; i < dropdowns.length; i++ ){
+//  dropdowns[i].classList.toggle('show');
+// }
+//
+//  this.children[1].classList.toggle('show');
+//
+//
+//  for( i = 0; i < menuDrop.length; i ++){
+//      menuDrop[i].addEventListener("mouseenter", toggleDropdown)
+//           menuDrop[i].addEventListener("mouseleave", toggleDropdown)
+// }
